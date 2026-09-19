797066aa15b29e4930baece532a326f8ae75b90b
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-19 09:05:17 +0530

    Day 1 and Day 2 all files

--- ML/DAY2AI.py
+++ ML/DAY2AI.py
@@ -98,5 +98,12 @@ print("count of vowels =" , count)
    # print(i)
 
 
-for i in range(2, 100 , 2):
-    print(i)
+#for i in range(2, 100 , 2):
+ #   print(i)
+
+
+n = int(input("enter the number:"))
+sum = 0
+for i in range(1 ,n+1):
+    sum += i
+print(sum)
