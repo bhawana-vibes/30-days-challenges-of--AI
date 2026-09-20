6c59739a2b8176987e14cbebfa361b54a91ff32e
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-20 19:24:12 +0530

    day3 coding file

--- ML/DAY3AI.py
+++ ML/DAY3AI.py
@@ -18,3 +18,16 @@ print(name[0:])
 print(name[1:len(name)])
 print(name[::])
 print(name[-5:-1])
+
+#string formatting
+a= 3
+b = 5
+sum = a+b
+#normal formatting
+print("sum of {} & {} is {}".format(a, b ,sum))
+
+#index based
+print("sum is {2} of {0} & {1}".format(a,b,sum))
+
+#value based
+print("values of  vars {a} & {b}".format(a=3 , b=2))
