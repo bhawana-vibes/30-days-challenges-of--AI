8d3242821538b03edbeab9bdd5153a8fd10f321d
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-22 20:45:50 +0530

    assignment 3

--- ML/assignment3.py
+++ ML/assignment3.py
@@ -1,6 +1,11 @@
 #que1
-word = input("enter the word:")
+'''word = input("enter the word:")
 if word == word[::-1]:
     print(" yes palindrome:" , word)
 else:
-    print("not palindrome:" , word)
+    print("not palindrome:" , word)'''
+
+#que2
+number = [10,30,40,45,56,60]
+avg = sum(number) / len(number)
+print(avg)
