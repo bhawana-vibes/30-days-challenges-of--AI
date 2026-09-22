72c5c443446416b1cb956aeeae0f4b0ac948fd20
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-22 21:01:56 +0530

    assignment3

--- ML/assignment3.py
+++ ML/assignment3.py
@@ -6,6 +6,15 @@ else:
     print("not palindrome:" , word)'''
 
 #que2
-number = [10,30,40,45,56,60]
+'''number = [10,30,40,45,56,60]
 avg = sum(number) / len(number)
-print(avg)
+print(avg)'''
+
+#que3
+#list1 = list[int(input("enter the numbers:"))]
+#list2 = list[int(input("enter the numbers:"))]
+list1 = [1,2,7]
+list2 = [2,4,5]
+merge_list = list1+list2
+merge_list.sort()
+print(merge_list)
