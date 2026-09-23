fd649371360ce0e58dcaaf99c3b75e17403017d2
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-23 13:59:02 +0530

    assignment3

--- ML/assignment3.py
+++ ML/assignment3.py
@@ -11,16 +11,16 @@ avg = sum(number) / len(number)
 print(avg)'''
 
 #que3
-#list1 = list[int(input("enter the numbers:"))]
+'''#list1 = list[int(input("enter the numbers:"))]
 #list2 = list[int(input("enter the numbers:"))]
 list1 = [1,2,7]
 list2 = [2,4,5]
 merge_list = list1+list2
 merge_list.sort()
-print(merge_list)
+print(merge_list)'''
 
 #que4
-tup = (2,4,6,7,8,9,11,23,15,18)
+'''tup = (2,4,6,7,8,9,11,23,15,18)
 even_list = []
 odd_list = []
 for num in tup:
@@ -31,4 +31,16 @@ for num in tup:
 even_tuple = tuple(even_list)
 odd_tuple = tuple(odd_list)
 print(even_tuple)
-print(odd_tuple)
+print(odd_tuple)'''
+
+'''#que6
+words = ["apple", "banana", "kiwi", "cherry", "mango"]
+dict = {}
+for w in words:
+    dict[w] = len(w)
+print(dict)'''
+
+#que7
+text = input("enter the text:")
+spaces = text.count(" ")
+print(spaces)
