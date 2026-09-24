aa51b8bfe891af8c7e7038853c73dbd61d1d1968
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-24 13:26:53 +0530

    practice

--- ML/mypractice.py
+++ ML/mypractice.py
@@ -1,4 +1,4 @@
-attempts = 0
+'''attempts = 0
 while attempts<3:
     pin = int(input("enter the pin:"))
     if pin == 1234:
@@ -8,4 +8,16 @@ while attempts<3:
         print("wrong pin .try again.")
         attempts += 1
 if attempts == 3:
-    print ("card blocked")
+    print ("card blocked")'''
+
+age = int(input("enter the age:"))
+day = input("enter the day:")
+if age<12:
+    price = 100
+elif age<60:
+    price = 200
+else:
+    price = 150
+if day =="saturday":
+   price = price-50
+print("final ticket price:rs.", price)
