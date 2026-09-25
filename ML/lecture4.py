011ba0de44ae277640c412e0c924fa458456ac5a
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-25 20:33:06 +0530

    lecture4

--- ML/lecture4.py
+++ ML/lecture4.py
@@ -1,1 +1,10 @@
 print("lecture4 start")
+
+class student:
+    name = "bhawana"
+    college = "abc"
+    year = "4th"
+stu1 = student()
+stu2 = student()
+print(stu1.name , stu1.year , stu1.college)
+print(stu2.name , stu2.year , stu2.college)
