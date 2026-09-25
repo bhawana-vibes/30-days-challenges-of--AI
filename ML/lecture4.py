a8b34b08048ad43b83394e612f38f6397bfdef9e
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-09-25 21:12:34 +0530

    lecture4

--- ML/lecture4.py
+++ ML/lecture4.py
@@ -11,11 +11,21 @@ print(type(l))
 print(type(stu1))
 print(type(stu2))
 #print(stu1.name , stu1.year , stu1.college)
-#print(stu2.name , stu2.year , stu2.college)'''
+#print(stu2.name , stu2.year , stu2.college)
 
 
 class frutis:
     def __init__(self):
         print("this is calling contructor")
 fru1 = frutis()
-fru2 = frutis()
+fru2 = frutis()'''
+
+
+class frutis:
+    def __init__(self , name):
+        self.name = name
+fru1 = frutis("apple")
+fru2 = frutis("mango")
+
+print(fru1.name)
+print(fru2.name)
